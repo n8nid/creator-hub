02e7d148abc5403da02772df41912f313934f3b8
Ficky Irwanto <ficky.irwanto@gmail.com>
2025-06-14 13:37:03 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: '.next',
-  experimental: {
-    // appDir is now enabled by default in Next.js 13+
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -13,7 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'standalone', // Required for Docker deployment
 };
 
 export default nextConfig; 
